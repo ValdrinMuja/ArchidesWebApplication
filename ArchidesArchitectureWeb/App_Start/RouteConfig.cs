aafe37c68af751391c0f5343436a52f287f10d94
Fisnik <Fisnik@LENOVO-PC>
2017-12-02 04:11:39 +0100

    UserInterface tek Create User

--- ArchidesArchitectureWeb/App_Start/RouteConfig.cs
+++ ArchidesArchitectureWeb/App_Start/RouteConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace ArchidesArchitectureWeb
@@ -12,6 +13,7 @@ namespace ArchidesArchitectureWeb
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+         
 
             routes.MapRoute(
                 name: "Default",
